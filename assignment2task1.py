5fd3d08cb8df1509ae1e16d2b7f246f9bdc57a74
vipulshu123 <vipul2799shukla@gmail.com>
2025-04-15 03:08:26 -0700

    Add files via upload

---
+++ assignment2task1.py
@@ -0,0 +1,6 @@
+x=int(input("write a number: "))
+y=x%2
+if y>0:
+    print("this is odd number")
+else:
+    print("this is even number")
---
+++ assignment2task2.py
@@ -0,0 +1,5 @@
+x=int(input("write a num: "))
+y=0
+for a in range(1,x+1):
+    y+=a
+print(y)
